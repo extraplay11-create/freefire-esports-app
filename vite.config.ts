b05db8683a67b9ce7e78f110617a586781f558bf
extraplay11-create <extraplay11@gmail.com>
2026-07-24 22:37:32 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+  base: "/freefire-esports-app/",
+  plugins: [react(), tailwindcss()],
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
