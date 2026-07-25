09b752d6c34ef5741348e22e33502ed0f564d418
extraplay11-create <extraplay11@gmail.com>
2026-07-25 22:51:14 +0530

    Update MyMatchesView.tsx

--- components/MyMatchesView.tsx
+++ components/MyMatchesView.tsx
@@ -14,7 +14,7 @@ import {
   ShieldAlert,
   X
 } from 'lucide-react';
-import { useApp } from '../context/AppContext';
+import { useApp } from '../AppContext';
 import { MatchRegistration, Tournament } from '../types';
 
 export const MyMatchesView: React.FC = () => {
