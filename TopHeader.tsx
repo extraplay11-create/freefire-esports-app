2e61c69488b1b25b07066465c80f3bd957778795
extraplay11-create <extraplay11@gmail.com>
2026-07-25 20:26:51 +0530

    Update TopHeader.tsx

--- TopHeader.tsx
+++ TopHeader.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Flame, Wallet, Shield, User as UserIcon, Smartphone, Monitor, Bell, Plus, RefreshCw, CheckCircle2 } from 'lucide-react';
-import { useApp } from '../AppContext';
+import { useApp } from '../context/AppContext';
 
 interface TopHeaderProps {
   onOpenAuth: () => void;
